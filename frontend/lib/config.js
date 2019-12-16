9ee3908a1a463a1254ba7148da8eddb6ca998f02
Evan Shortiss <evanshortiss@gmail.com>
2019-12-16 09:03:47 -0800

    fix: use random session secret on restarts

--- frontend/lib/config.js
+++ frontend/lib/config.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const env = require('env-var')
+const { randomBytes } = require('crypto')
 
 module.exports = {
   AMQP: {
@@ -11,8 +12,11 @@ module.exports = {
   },
   THREESCALE_HEADER_NAME: env.get('THREESCALE_HEADER_NAME', 'x-3scale-proxy-secret-token').asString(),
   THREESCALE_SECRET: env.get('THREESCALE_SECRET').asString(),
+  // This is the OIDC JSON from the installation tab of a keycloak client
   KEYCLOAK_CONFIG: env.get('KEYCLOAK_CONFIG').asJsonObject(),
-  SESSION_SECRET: env.get('SESSION_SECRET', 'such secret. very secure. wow').asString(),
+  // Use a random session secret unless one is specified
+  SESSION_SECRET: env.get('SESSION_SECRET', randomBytes(8).toString('hex')).asString(),
+  // Set this to "development" or "production"
   NODE_ENV: env.get('NODE_ENV').asString(),
   LOG_LEVEL: env.get('LOG_LEVEL', 'debug').asEnum(['trace', 'debug', 'info'])
 }
